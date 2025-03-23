49e59fe2bac6e033ba0d0bb8e05a61b13512905c
glowfi <86939591+glowfi@users.noreply.github.com>
2025-03-23 17:50:16 +0530

    remove axios hook and global axios instance

---
+++ frontend/src/axios/axios-instance.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+
+export default function axiosInstance(
+    config: AxiosRequestConfig = {}
+): AxiosInstance {
+    axios.defaults.baseURL = process.env.API_BASE_URL;
+
+    return axios.create(config);
+}
