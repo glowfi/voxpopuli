11d9470343eb59dc54512b50ab4287f85ab8c3f0
glowfi <86939591+glowfi@users.noreply.github.com>
2025-03-23 15:53:09 +0530

    add axios and useAxios hook

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
     env: {
-        API_ENDPOINT: process.env.API_ENDPOINT,
+        API_BASE_URL: process.env.API_BASE_URL,
         APP_NAME: process.env.APP_NAME,
         APP_MOTTO: process.env.APP_MOTTO
     },
---
+++ frontend/src/hooks/useAxios.ts
@@ -0,0 +1,75 @@
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
+
+axios.defaults.baseURL = process.env.API_BASE_URL;
+
+type Method = 'GET' | 'POST' | 'HEAD' | 'PUT' | 'PATCH' | 'DELETE';
+type ResponseType = 'arraybuffer' | 'document' | 'json' | 'text' | 'stream';
+
+export interface UseAxiosReturns {
+    data: AxiosResponse | null;
+    status: number | null;
+    error: AxiosError | null;
+    loading: boolean;
+}
+
+export interface UseAxiosArgs {
+    url: string;
+    method: Method;
+    baseURL?: string;
+    headers?: { [key: string]: string };
+    params?: { [key: string]: string | number | boolean };
+    payload?: string | number | boolean | object | null;
+    timeout?: number;
+    withCredentials?: boolean;
+    responseType?: ResponseType;
+}
+
+export default function useAxios({
+    url,
+    method,
+    baseURL,
+    headers,
+    params,
+    payload,
+    timeout,
+    withCredentials,
+    responseType
+}: UseAxiosArgs) {
+    const [data, setData] = useState<AxiosResponse | null>(null);
+    const [status, setStatus] = useState<number | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<AxiosError | null>(null);
+
+    useEffect(() => {
+        axios({
+            url,
+            method,
+            baseURL,
+            headers,
+            params,
+            data: payload,
+            timeout,
+            withCredentials,
+            responseType
+        })
+            .then(({ data, status }: AxiosResponse) => {
+                setData(data);
+                setStatus(status);
+            })
+            .catch((error: AxiosError) => setError(error))
+            .finally(() => setLoading(false));
+    }, [
+        url,
+        method,
+        baseURL,
+        headers,
+        params,
+        payload,
+        timeout,
+        withCredentials,
+        responseType
+    ]);
+
+    return { data, status, error, loading };
+}
