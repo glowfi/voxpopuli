8ae17a78d852c58e0a8ee34d4fb4b9041016855b
glowfi <86939591+glowfi@users.noreply.github.com>
2025-03-24 22:08:04 +0530

    revert back to old logging middleware

--- backend/internal/middleware/logging.go
+++ backend/internal/middleware/logging.go
@@ -28,22 +28,6 @@ func getRealIP(r *http.Request) string {
 	return realIP
 }
 
-// colorize returns a colored string representation of the status code.
-func colorize(statusCode int) string {
-	switch {
-	case statusCode >= 200 && statusCode < 300:
-		return "\033[32m" + string(rune(statusCode)) + "\033[0m" // Green for 2xx
-	case statusCode >= 300 && statusCode < 400:
-		return "\033[36m" + string(rune(statusCode)) + "\033[0m" // Cyan for 3xx
-	case statusCode >= 400 && statusCode < 500:
-		return "\033[33m" + string(rune(statusCode)) + "\033[0m" // Yellow for 4xx
-	case statusCode >= 500:
-		return "\033[31m" + string(rune(statusCode)) + "\033[0m" // Red for 5xx
-	default:
-		return string(rune(statusCode))
-	}
-}
-
 func Logging(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
@@ -54,6 +38,6 @@ func Logging(next http.Handler) http.Handler {
 
 		next.ServeHTTP(wrapped, r)
 
-		log.Printf("%s %s %s %s %v\n", colorize(wrapped.statusCode), r.Method, r.URL.Path, getRealIP(r), time.Since(start))
+		log.Println(wrapped.statusCode, r.Method, r.URL.Path, getRealIP(r), time.Since(start))
 	})
 }
