f7025b86f6fa1ce1478e2d4cfb740bde7d681ede
glowfi <86939591+glowfi@users.noreply.github.com>
2025-03-24 21:57:41 +0530

    add a small axios instance helper

--- frontend/src/axios/axios-instance.ts
+++
@@ -1,9 +0,0 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
-
-export default function axiosInstance(
-    config: AxiosRequestConfig = {}
-): AxiosInstance {
-    axios.defaults.baseURL = process.env.API_BASE_URL;
-
-    return axios.create(config);
-}
